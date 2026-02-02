28b0c7cb900520b31bdd2cb4a2f3ceee7d4d45ec
DeveloperWide <maheshisdelta@gmail.com>
2026-02-02 21:04:33 +0530

    removed / from origin field in cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,10 @@ import authRoutes from "./routes/auth.route";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173/"],
-    credentials: true,
+    origin: ["http://localhost:5173"],
   }),
 );
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
